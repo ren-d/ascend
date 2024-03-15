ae22099ec2586e7fbf606e6913ff7a11d5c61440
Duncan Rendall <duncansrendall@gmail.com>
2024-03-15 14:49:02 +0000

    Enabled Debug Layer to test D3D12 linked correctly

--- src/ascend/main.cpp
+++ src/ascend/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <d3d12.h>
+#include <wrl.h>
+using namespace Microsoft::WRL;
 
 int main()
 {
+	ComPtr<ID3D12Debug> DebugController;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&DebugController))))
+	{
+		DebugController->EnableDebugLayer();
+		std::cout << "D3D12 Debug Layer Has Been Enabled" << std::endl;
+	}
+
 	std::cout << "ascend." << std::endl;
 	return 0;
 }
