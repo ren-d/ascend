f61c45641c38e942ee028f05ca065525f988c05a
Duncan R <duncansrendall@gmail.com>
2024-04-18 00:25:11 +0100

    https://github.com/KStocky/ShaderTestFramework NugetPackageManager for easy Agility_SDK & D3D12 Helper Library cmake integration

--- src/ascend/WindowsApplication.h
+++ src/ascend/WindowsApplication.h
@@ -8,7 +8,7 @@ class WindowsApplication
 {
 public:
 	 static int Run(HINSTANCE hInstance, int nCmdShow);
-	//static HWND GetHwnd() { return m_hwnd; }
+	 static HWND GetHwnd() { return m_hwnd; }
 
 protected:
 	 static LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
--- src/ascend/main.cpp
+++ src/ascend/main.cpp
@@ -2,18 +2,11 @@
 #include <d3d12.h>
 #include <wrl.h>
 #include "WindowsApplication.h"
-using namespace Microsoft::WRL;
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
 	WindowsApplication::Run(hInstance, nCmdShow);
-	ComPtr<ID3D12Debug> DebugController;
-	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&DebugController))))
-	{
-		DebugController->EnableDebugLayer();
-		std::cout << "D3D12 Debug Layer Has Been Enabled" << std::endl;
-	}
-
 	std::cout << "ascend." << std::endl;
 	return 0;
 }
